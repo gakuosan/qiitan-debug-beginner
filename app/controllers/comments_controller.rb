59dbc871833ff5b2e420c0da33792cdc35c0a56d
gakuchan <gakuo19831201@gmail.com>
2019-06-30 11:20:27 +0900

    コメントを登録変更ができるように変更

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -35,6 +35,7 @@ class CommentsController < ApplicationController
 
   def comment_params
     #params.require(:comment).permit(:title)
+    #コメントを登録変更ができるようにする
     params.require(:comment).permit(:body)
   end
 end
